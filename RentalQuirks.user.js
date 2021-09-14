674f6c700c7aa7f3a89ce20e742dafbb71ad89fe
acropup <acropup@users.noreply.github.com>
2021-09-13 19:10:37 -0700

    Update for new domain (rentalworksweb.com)
    We were moved from dbwcloud.com to rentalworksweb.com, so this updates the URL matching to accept both domains.
    
    Also added names to pinned main menu icons.

--- RentalQuirks.user.js
+++ RentalQuirks.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         RentalQuirks - Feature enhancements for RentalWorks Web
 // @namespace    https://www.github.com/acropup/RentalQuirks/
-// @version      0.4
-// @description  RentalQuirks is a collection of Javascript and CSS customizations that improve usability, layout, and features of the web-based "RentalWorks" rental management software at dbwcloud.com
+// @version      0.5
+// @description  RentalQuirks is a collection of Javascript and CSS customizations that improve usability, layout, and features of the web-based "RentalWorks" rental management software at rentalworksweb.com
 // @homepageURL  https://www.github.com/acropup/RentalQuirks
 // @author       Shane Burgess
 // @match        *://*.dbwcloud.com/*
+// @match        *://*.rentalworksweb.com/*
 // @grant        none
 // @noframes
 // @require      https://raw.githubusercontent.com/acropup/RentalQuirks/master/js/script_execution_mgr.js
