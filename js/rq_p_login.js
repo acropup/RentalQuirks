b79d6af36638424ae23126f4f26a8e9a84e77900
Shane Burgess <acropup@users.noreply.github.com>
2023-04-18 15:34:49 -0700

    Auto-login robustness
    Chrome errors if you qS() an :autofill, so it requires :-webkit-autofill. Firefox errors if you use the -webkit- prefix. This tries to catch both, but also bails out if an error is still generated somehow.

--- js/rq_p_login.js
+++ js/rq_p_login.js
@@ -17,7 +17,7 @@
         const entry = "/default";
         const login = "/login";
         if (h.endsWith(entry)) {
-            location.href = h.substr(0, h.length - entry.length) + login;
+            location.href = h.substring(0, h.length - entry.length) + login;
         }
     }
 
@@ -29,8 +29,10 @@
         // Abort autologin if there is an error message
         let errorDiv = document.querySelector(".errormessage");
         if (errorDiv?.offsetParent) return true;
+        // Abort autologin if there is an error popup
+        if (document.querySelector(".errorDialog")) return true;
 
-        let loginButton = document.querySelector(".login-button[data-id]");
+        let loginButton = document.querySelector(".login-button.btnLogin");
         // Check if login button exists and is visible (offsetParent == null implies element is hidden)
         if (loginButton?.offsetParent) {
             let unField = document.querySelector(".login-fields #email");
@@ -39,8 +41,27 @@
             let pwLength = pwField?.value.length ?? 0;
             // Chrome password manager doesn't expose autofilled password values to javascript without 
             // user interaction, but we can check for the existence of :autofill pseudo-class.
-            let unAutofilled = !!document.querySelector(".login-fields #email:autofill");
-            let pwAutofilled = !!document.querySelector(".login-fields #password:autofill");
+            let unAutofilled = false;
+            let pwAutofilled = false;
+
+            try {
+                // Chrome, Edge, and Opera require the -webkit- prefix (April 2023)
+                if (/edg|opr|chrome|chromium|crios/i.test(window.navigator.userAgent)) {
+                    unAutofilled = !!document.querySelector(".login-fields #email:-webkit-autofill");
+                    pwAutofilled = !!document.querySelector(".login-fields #password:-webkit-autofill");
+                }
+                else {
+                    unAutofilled = !!document.querySelector(".login-fields #email:autofill");
+                    pwAutofilled = !!document.querySelector(".login-fields #password:autofill");
+                }
+            }
+            catch (error) {
+                // Performing querySelector with an invalid pseudo-element results in an error, like:
+                //   Uncaught DOMException: Failed to execute 'querySelector' on 'Document': '.login-fields #email:autofill' is not a valid selector.
+                console.warn(error);
+                // If we catch an error, it is a sign of a browser incompatibility. In that case, return true so that this script isn't run again.
+                return true;
+            }
 
             // The expectation is that a password manager will autofill these fields, but we
             // have to make sure not to interrupt the user if they're typing it manually.
@@ -48,7 +69,8 @@
                 (pwAutofilled || (pwLength > 0 && document.activeElement != pwField))) {
                 console.log('RentalQuirks auto-login');
                 loginButton.click();
-                return true;
+                // Keep trying until the page changes, because calling click() is ignored until the page has had some user interaction.
+                return false;
             }
         }
         return false;
